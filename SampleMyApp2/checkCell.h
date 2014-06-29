8f290f8b358354588bd1b6a507d8a022ba7bd1a7
Kayok_Hashi <mo.chuiske.usi@gmail.com>
2014-06-29 16:38:52 +0800

    カスタムセルの作成中

---
+++ SampleMyApp2/checkCell.h
@@ -0,0 +1,13 @@
+//
+//  checkCell.h
+//  SampleMyApp2
+//
+//  Created by 橋香代子 on 2014/06/29.
+//  Copyright (c) 2014年 Kayoko Hashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface checkCell : UITableViewCell
+
+@end
---
+++ SampleMyApp2/checkCell.m
@@ -0,0 +1,25 @@
+//
+//  checkCell.m
+//  SampleMyApp2
+//
+//  Created by 橋香代子 on 2014/06/29.
+//  Copyright (c) 2014年 Kayoko Hashi. All rights reserved.
+//
+
+#import "checkCell.h"
+
+@implementation checkCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
